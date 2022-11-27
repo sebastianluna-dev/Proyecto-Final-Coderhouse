8ec74353278e048070c65966d6871a53360b8504
sebastianluna-dev <sebastian.luna.developer@gmail.com>
2022-11-27 00:37:24 -0600

    port variable fixed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const DB_USERNAME = process.env.DB_USERNAME || "sebastian"
 const DB_PASSWORD = process.env.DB_PASSWORD || "naitsabes123"
-const PORT = process.env.$PORT || 8080
+const PORT = process.env.PORT || 8080
 
 export { DB_USERNAME, DB_PASSWORD, PORT }
